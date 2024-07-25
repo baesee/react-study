0e519f091eab68c7737264fb20c86829fc701edc
baesee <baesee@baeseeui-MacBookAir.local>
2024-07-25 13:50:11 +0900

    useReducer 기본 학습, useState를 대체할 수 있음

---
+++ section09/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState, useRef } from 'react';
+import './App.css';
+import Header from './components/Header';
+import List from './components/List';
+import Editor from './components/Editor';
+import Exam from './components/Exam';
+
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content: 'React 공부하기',
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: '빨래하기',
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: '집 청소하기',
+        date: new Date().getTime(),
+    },
+];
+
+function App() {
+    const [todos, setTodos] = useState(mockData);
+    const idRef = useRef(3);
+
+    const onCreate = (content) => {
+        const newTodo = {
+            id: idRef.current++,
+            isDone: false,
+            content: content,
+            date: new Date().getTime(),
+        };
+        setTodos([newTodo, ...todos]);
+    };
+
+    const onUpdate = (targetId) => {
+        // todos State의 값들 중에
+        // targetId와 일치하는 id를 갖는 todo 아이템의 isDone을 toggle
+        // 그리고 새로운 배열을 만들어서 todos State를 업데이트
+        const newTodos = todos.map((todo) =>
+            todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+        );
+        setTodos(newTodos);
+    };
+
+    const onDelete = (targetId) => {
+        setTodos(todos.filter((todo) => todo.id !== targetId));
+    };
+
+    return (
+        <div className="App">
+            <Exam />
+            {/* <Header />
+            <Editor onCreate={onCreate} />
+            <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+        </div>
+    );
+}
+
+export default App;
